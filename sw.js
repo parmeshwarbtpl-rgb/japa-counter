18f208d044c21a27f660b263ffb299fd0d70aab2
parmeshwarbtpl-rgb <parmeshwar.btpl@gmail.com>
2026-07-13 11:24:19 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = "naam-jaap-counter-v2-1";
+const APP_SHELL = [
+    "./",
+    "./index.html",
+    "./css/style.css",
+    "./js/api.js",
+    "./js/settings.js",
+    "./js/history.js",
+    "./js/ui.js",
+    "./js/app.js",
+    "./manifest.json",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(APP_SHELL))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys()
+            .then(keys => Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            ))
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", event => {
+    const requestUrl = new URL(event.request.url);
+
+    if (event.request.method !== "GET") return;
+
+    if (requestUrl.hostname.includes("script.google.com") || requestUrl.hostname.includes("script.googleusercontent.com")) {
+        event.respondWith(fetch(event.request));
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) return cachedResponse;
+
+            return fetch(event.request).then(networkResponse => {
+                if (!networkResponse || networkResponse.status !== 200 || networkResponse.type === "opaque") {
+                    return networkResponse;
+                }
+
+                const responseClone = networkResponse.clone();
+                caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
+                return networkResponse;
+            });
+        })
+    );
+});
