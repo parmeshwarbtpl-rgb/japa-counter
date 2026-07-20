9c2195084ce52f653206a88e768412b3cadfcf1d
parmeshwarbtpl-rgb <parmeshwar.btpl@gmail.com>
2026-07-20 10:30:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,12 @@
 // never place a client secret here.
 
 window.APP_CONFIG = Object.freeze({
-    API_URL: "https://script.google.com/macros/s/AKfycbzfUW7ADdZfzE82PEsE5czOLdGuTlY4S1SEb_698IX-4ti1-l4aWXdLBUh1nMOc2L4s/exec",
+    API_URL:
+        "https://script.google.com/macros/s/AKfycbzfUW7ADdZfzE82PEsE5czOLdGuTlY4S1SEb_698IX-4ti1-l4aWXdLBUh1nMOc2L4s/exec",
 
-    GOOGLE_CLIENT_ID: "984438160673-ru9ed5qrbh4hrrl63lpliaakn6da6i87.apps.googleusercontent.com",
+    GOOGLE_CLIENT_ID:
+        "205720505540-ncjatpfsvr91pcebl9mkd5obum3cjf62.apps.googleusercontent.com",
 
-    APP_VERSION: "2.8.2-custom-mantras"
+    APP_VERSION:
+        "2.8.2-custom-mantras"
 });
